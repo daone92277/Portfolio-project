91c8d3dcdcd4a9b878c96eb784326590f9d7141a
Greene <David.Greene@broadridge.com>
2019-09-01 09:29:10 -0400

    blog page created and static folder built

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Blog
 
 # Create your views here.
 def blog_home(request):
-     return render(request, "blogs/blog.html")
+    blogs = Blog.objects
+    return render(request, "blogs/blog.html", {"blog":blogs})
+
+def detail(request, blog_id):
+    blogDetail = get_object_or_404(Blog, pk=blog_id)
+
+    return render(request, "blogs/detail.html", {"blog":blogDetail})
