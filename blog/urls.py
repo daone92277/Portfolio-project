337931b43149c6788b596fab48c126dce1a09494
Greene <David.Greene@broadridge.com>
2019-09-07 08:14:00 -0400

    fixed blog and home links

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.blog_home, name="blog"),
+    path("", views.blog_home, name="blog_url"),
     path("<int:blog_id>/",views.detail, name="detail")
    
 ]
+
+  
--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .models import Blog
 # Create your views here.
 def blog_home(request):
     blogs = Blog.objects
-    return render(request, "blogs/blog.html", {"blog":blogs})
+    return render(request, "blogs/blog.html", {"blogs":blogs})
 
 def detail(request, blog_id):
     blogDetail = get_object_or_404(Blog, pk=blog_id)
